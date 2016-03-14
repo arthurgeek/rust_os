62f43b3a167da19ad131309f4bdb3938265be94b
Arthur Zapparoli <arthurzap@gmail.com>
2016-03-13 23:41:17 -0300

    Changing assembly message printed on screen

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -6,18 +6,13 @@ long_mode_start:
   extern rust_main
   call rust_main
 
-  ; print Hello World! to the screen
-  mov word [0xb8000], 0x0148 ; H
-  mov word [0xb8002], 0x0265 ; e
-  mov word [0xb8004], 0x036c ; l
-  mov word [0xb8006], 0x046c ; l
-  mov word [0xb8008], 0x056f ; o
-  mov word [0xb800a], 0x0220 ;  
-  mov word [0xb800c], 0x0677 ; w
-  mov word [0xb800e], 0x076f ; o
-  mov word [0xb8010], 0x0872 ; r
-  mov word [0xb8012], 0x096c ; l
-  mov word [0xb8014], 0x0e64 ; d
-  mov word [0xb8016], 0x0f21 ; !
+.os_returned:
+  ; rust main returned, print `OS returned!`
+  mov rax, 0x4f724f204f534f4f ; OS r
+  mov [0xb8000], rax
+  mov rax, 0x4f724f754f744f65 ; etur
+  mov [0xb8008], rax
+  mov rax, 0x4f214f644f654f6e ; ned!
+  mov [0xb8010], rax
 
   hlt
