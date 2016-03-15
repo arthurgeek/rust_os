0a5931ab80a3eb868aea46a95a77d8982be56786
Arthur Zapparoli <arthurzap@gmail.com>
2016-03-14 23:49:09 -0300

    We better use our VGA text buffer interface

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,6 @@ mod vga_buffer;
 #[no_mangle]
 pub extern fn rust_main() {
     // ATTENTION: we have a very small stack and no guard page
-    let hello = b"\x02\x01 Hello World! \x01\x02";
-    let color_byte = 0x1f; // white fg, blue bg
-
-    let mut hello_colored = [color_byte; 36];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
     vga_buffer::clear_screen();
     println!("It works{}", "!");
 
