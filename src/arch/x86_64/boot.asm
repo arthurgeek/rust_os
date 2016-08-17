6888f3c28c2ad0bd5d9531e97ba59c832876a6c3
Arthur Zapparoli <arthur.zapparoli@askcharlie.com>
2016-08-18 00:11:00 +0200

    Making the jump to long mode!

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -15,19 +15,8 @@ start:
   mov ds, ax ; data selector
   mov es, ax ; extra selector
 
-  mov word [0xb8000], 0x0148 ; H
-  mov word [0xb8002], 0x0265 ; e
-  mov word [0xb8004], 0x036c ; l
-  mov word [0xb8006], 0x046c ; l
-  mov word [0xb8008], 0x056f ; o
-  mov word [0xb800a], 0x0220 ;  
-  mov word [0xb800c], 0x0677 ; w
-  mov word [0xb800e], 0x076f ; o
-  mov word [0xb8010], 0x0872 ; r
-  mov word [0xb8012], 0x096c ; l
-  mov word [0xb8014], 0x0e64 ; d
-  mov word [0xb8016], 0x0f21 ; !
-  hlt
+  ; far jumping to long mode!
+  jmp gdt64.code:long_mode_start
 
 set_up_page_tables:
   ; point the first entry of level 4 page table to the first entry in the
@@ -100,3 +89,20 @@ gdt64:
 .pointer:
   dw .pointer - gdt64 - 1
   dq gdt64
+
+section .text
+bits 64
+long_mode_start:
+  mov word [0xb8000], 0x0148 ; H
+  mov word [0xb8002], 0x0265 ; e
+  mov word [0xb8004], 0x036c ; l
+  mov word [0xb8006], 0x046c ; l
+  mov word [0xb8008], 0x056f ; o
+  mov word [0xb800a], 0x0220 ;  
+  mov word [0xb800c], 0x0677 ; w
+  mov word [0xb800e], 0x076f ; o
+  mov word [0xb8010], 0x0872 ; r
+  mov word [0xb8012], 0x096c ; l
+  mov word [0xb8014], 0x0e64 ; d
+  mov word [0xb8016], 0x0f21 ; !
+  hlt
